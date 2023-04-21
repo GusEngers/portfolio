5e2fabf5db7b709707e45c7b3776903193391234
GusEngers <gustavoengers6441@gmail.com>
2023-04-20 22:27:41 -0300

    Inicio porfolio - Componente Navbar

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/components/Navbar/Navbar';
+
+export default function About(): JSX.Element {
+  return (
+    <div>
+      <Navbar />
+      <h1>hola</h1>
+    </div>
+  );
+}
