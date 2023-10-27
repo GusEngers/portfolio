fa91aa95930e6de7e869d5dc89ed385dc7da1366
Gustavo Engers <gustavoengers6441@gmail.com>
2023-10-26 23:13:08 -0300

    Add: Rendering pages with error

--- src/middlewares/verify-type.js
+++ src/middlewares/verify-type.js
@@ -4,17 +4,15 @@ const { TYPES } = require('../utils/constants');
 module.exports = {
   verifyId: (req, res, next) => {
     if (!isObjectIdOrHexString(req.params.id)) {
-      // return res.render('error', {
-      //   error: 'El id proporcionado no tiene un formato válido',
-      // });
-      res.json("id no valido")
+      return res.render('pages/error', {
+        error: 'El id proporcionado no tiene un formato válido',
+      });
     }
     next();
   },
   verifyType: (req, res, next) => {
     if (!TYPES.includes(req.params.type)) {
-      // return res.render('not-found');
-      return res.json("pagina no valida")
+      return res.render('pages/not-found');
     }
     next();
   },
