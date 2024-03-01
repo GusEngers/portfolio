a640ae56516f74bd6aff99c9ce2d546629773f09
GusEngers <gustavoengers6441@gmail.com>
2024-03-01 19:05:20 -0300

    home

--- src/app.js
+++ src/app.js
@@ -13,13 +13,13 @@ app.use(require('morgan')('dev'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views/pages');
+app.set('views', path.join(__dirname, 'views', 'pages'));
 
 app.use(handleCors);
 
 app.use('/', router);
 
-app.use(handleError);
+// app.use(handleError);
 app.use(handleNotFound);
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,15 @@ const pt = require('./pt');
 
 const { homeRedirect } = require('../controllers');
 
-router.route('/').get(homeRedirect);
+// router.route('/').get(homeRedirect);
+router.route('/').get((req, res) => {
+  const techs = [{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},
+  {name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},
+  {name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},
+  {name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},
+  {name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},{name: "javascript", icon: "logos:javascript"},]
+  res.render('home', {techs});
+});
 router.use('/es', es);
 router.use('/pt', pt);
 
