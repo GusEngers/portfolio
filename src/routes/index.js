4cdf32d5a85767da32b46fa314d78d2205b4b8fb
GusEngers <gustavoengers6441@gmail.com>
2024-03-02 01:00:33 -0300

    Footer y colores de fondo

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,6 +20,51 @@ router.route('/').get((req, res) => {
   ];
   res.render('home', { techs });
 });
+router.route('/about').get((req, res) => {
+  const techs = [
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' }
+  ];
+  res.render('home', { techs });
+});
+router.route('/projects').get((req, res) => {
+  const techs = [
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' }
+  ];
+  res.render('home', { techs });
+});
+router.route('/projects/1').get((req, res) => {
+  const techs = [
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' },
+    { _id: 1, name: 'javascript', icon: 'logos:javascript' }
+  ];
+  res.render('home', { techs });
+});
 router.use('/es', es);
 router.use('/pt', pt);
 
